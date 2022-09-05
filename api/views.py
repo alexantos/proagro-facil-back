24378e9ae87aa961514e98cbad6b85029782b5c1
Xangool <xandreale09@gmail.com>
2022-09-05 03:02:23 -0300

    Add logic test for distance <= 10Km localization

--- api/views.py
+++ api/views.py
@@ -14,7 +14,6 @@ class ComunicacaoPerdaViewSet(viewsets.ModelViewSet):
 
 @api_view(['GET'])
 def grafico(request):
-    print()
     graficos = [
         {"nome": "CHUVA EXCESSIVA", "quantidade": len(ComunicacaoPerda.objects.filter(evento_ocorrido="CHUVA EXCESSIVA"))},
         {"nome": "GEADA", "quantidade": len(ComunicacaoPerda.objects.filter(evento_ocorrido="GEADA"))},
@@ -24,3 +23,52 @@ def grafico(request):
         {"nome": "RAIO", "quantidade": len(ComunicacaoPerda.objects.filter(evento_ocorrido="RAIO"))},
     ]
     return Response(graficos)
+
+
+@api_view(['GET'])
+def verifica_distancia(request, id):
+    perda_parametro = ComunicacaoPerda.objects.get(id=id)
+    #Testar corretamente esse método
+    for perda_banco in ComunicacaoPerda.objects.filter(data_cadastro=perda_parametro.data_cadastro).exclude(evento_ocorrido=perda_parametro.evento_ocorrido):
+        distancia = getDistanceBetweenPointsNew(
+            latitude1=perda_parametro.latitude,
+            longitude1=perda_parametro.longitude,
+            latitude2=perda_banco.latitude,
+            longitude2=perda_banco.longitude,
+            unit='kilometers'
+        )
+        print(distancia)
+        if distancia <= 10:
+            print("Comunica analista")
+        else:
+            print("Segue o baile")
+    return Response({})
+
+
+from numpy import sin, cos, arccos, pi, round
+
+
+def rad2deg(radians):
+    degrees = radians * 180 / pi
+    return degrees
+
+
+def deg2rad(degrees):
+    radians = degrees * pi / 180
+    return radians
+
+
+def getDistanceBetweenPointsNew(latitude1, longitude1, latitude2, longitude2, unit='miles'):
+    theta = longitude1 - longitude2
+
+    distance = 60 * 1.1515 * rad2deg(
+        arccos(
+            (sin(deg2rad(latitude1)) * sin(deg2rad(latitude2))) +
+            (cos(deg2rad(latitude1)) * cos(deg2rad(latitude2)) * cos(deg2rad(theta)))
+        )
+    )
+
+    if unit == 'miles':
+        return round(distance, 2)
+    if unit == 'kilometers':
+        return round(distance * 1.609344, 2)
--- configuracao/urls.py
+++ configuracao/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('grafico/', grafico),
+    path('distancia/<int:id>/', verifica_distancia),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
